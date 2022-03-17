0251fde77ee4626651b20c4e6d5708890b1f6956
x0n4d0 <romagnolimauricio@gmail.com>
2022-03-17 02:10:03 -0300

    feat: add logger settings file

---
+++ server/util.js
@@ -0,0 +1,13 @@
+import pino, { transport } from 'pino';
+
+const log = pino({
+  enabled: !(!!process.env.LOG_DISABLED),
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true
+    }
+  }
+});
+
+export const logger = log;
