fd03c8ce6e2165ac58ce668b80c96fbe36804a50
x0n4d0 <romagnolimauricio@gmail.com>
2022-04-01 00:14:29 -0300

    chore: remove old files

--- server/index.js
+++
@@ -1,7 +0,0 @@
-import server from './server.js';
-
-import { logger } from './util.js';
-import config from './config.js';
-
-server.listen(config.port)
-  .on('listening', () => logger.info(`[*] Server running at ${config.port}.`));
